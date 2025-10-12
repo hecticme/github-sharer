a4d84d1aa102387a9635c93762c80bf6ac83bc43
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-10-12 11:55:19 +0700

    feat: add support for Github issue page

--- popup/popup.js
+++ popup/popup.js
@@ -4,14 +4,23 @@ const copyButtonElement = document.getElementById('copy')
 async function getPRInfo() {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
 
-  if (!tab.url.includes('github.com') || !tab.url.includes('/pull/')) {
-    outputElement.value = 'Not a GitHub PR page.'
+  if (!tab.url.includes('github.com')) {
+    outputElement.value = 'This extension only works on Github.'
+    return
+  }
+
+  const isPullRequest = tab.url.includes('/pull/')
+  const isIssue = tab.url.includes('/issues/')
+
+  if (!isPullRequest && !isIssue) {
+    outputElement.value = 'Must be a pull request or an issue.'
     return
   }
 
   const [{ result }] = await chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: () => {
+    args: [isPullRequest],
+    func: (isPullRequest) => {
       const pathParts = window.location.pathname.split('/')
       // github.com/owner/repo-name/pull/7/files --> ['', 'owner', 'repo-name', 'pull', '7', 'files']
       const repoName = pathParts.at(2) ?? ''
@@ -19,12 +28,13 @@ async function getPRInfo() {
         .replace(/[-_]/g, ' ')
         .replace(/\b\w/g, substring => substring.toUpperCase())
 
-      const titleElement = /** @type {HTMLElement} */ (document.querySelector('.gh-header-title .js-issue-title'))
+      const titleSelector = isPullRequest ? '.gh-header-title .js-issue-title' : '.markdown-title'
+      const titleElement = /** @type {HTMLElement} */ (document.querySelector(titleSelector))
       const title = titleElement ? titleElement.innerText.trim() : '(No title found)'
 
-      const pullRequestHref = `${window.location.origin}/${pathParts.slice(1, 5).join('/')}`
+      const referenceHref = `${window.location.origin}/${pathParts.slice(1, 5).join('/')}`
 
-      return `[Project] ${projectName}\n[Task]: review: ${title}\n[Assignee]\n[Reference] ${pullRequestHref}`
+      return `[Project] ${projectName}\n[Task]: review: ${title}\n[Assignee]\n[Reference] ${referenceHref}`
     },
   })
 
